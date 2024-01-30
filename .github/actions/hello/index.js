efc32a9c4c001d93f26c57b4a1247bb304bd8397
Juan Jose Sierra Mejia <juanjose.sierra@rockwellautomation.com>
2024-01-30 11:09:11 -0500

    hello world action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,25 @@
+import core from '@actions/core'
+import github from '@actions/github'
+
+try {
+
+    core.degub('Debug Message')
+    core.warning('Warning Message')
+    core.error('Error Message')
+
+    const name = core.getInput('who_to_greet')
+
+    console.log(`Hello ${name}`)
+
+    const time = new Date()
+    core.setOutput("time", time.toTimeString())
+
+    core.exportVariable("HELLO_TIME", time)
+
+    core.startGroup("Logging github context")
+    console.log(JSON.stringify(github.context, null, 2))
+    core.endGroup()
+
+} catch (e) {
+    core.setFailed(e.message)
+}
